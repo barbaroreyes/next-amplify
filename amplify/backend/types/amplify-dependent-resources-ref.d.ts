20eb93de570d8c8abb3a1b8c20edbb47983e3fa6
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-19 10:28:33 -0600

    update

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "test2": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
